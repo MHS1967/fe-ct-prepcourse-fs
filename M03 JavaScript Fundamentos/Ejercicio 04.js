a6b3e5d2605eba4ff34bca31eae9340447edd2e8
MHS1967 <mhscarano@hotmail.com>
2023-07-17 00:06:39 -0300

    tercer commit

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -24,17 +24,23 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
-   
+   return Math.abs(num) - Math.trunc(Math.abs(num)) < 0.2 ? Math.trunc(num) : Math.round(num,0);    
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
-}
-
+   if (num < -1) {return Math.trunc(num);}
+   else if (num > -1 && num <= 0 ) { return 0; }
+   else if (num > 0 && num <= 1 ) { return 1; }
+   else if (num - Math.trunc(num) < 0.5) { return Math.trunc(num)+1; }
+   else return Math.round(num); 
+ }
+ 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   return Math.random();
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
